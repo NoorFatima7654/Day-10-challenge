14d72ea3df1eeb586ace6440e1e27fc35ab591d4
Noor Fatima <160162835+NoorFatima7654@users.noreply.github.com>
2024-03-11 20:25:55 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,134 @@
+// Questioin no 28
+// if the person is less then 2 years old
+// let age: number = 1;
+// if (age < 2){
+//     console.log("The person is a Baby")
+// } else if (age < 4){
+//     console.log("The person is Toddler")
+// } else if (age < 13){
+//     console.log("The person is a kid")
+// } else if (age < 20){
+//     console.log("The person is a teenager ")
+// } else if (age < 65){
+//     console.log("The person is adult ")
+// } else {
+//     console.log("The person is Elder")
+// }
+// if the person is at least 2 years old but less than 4,
+// let age: number = 3;
+// if (age < 2){
+//     console.log("The person is a Baby")
+// } else if (age < 4){
+//     console.log("The person is Toddler")
+// } else if (age < 13){
+//     console.log("The person is a kid")
+// } else if (age < 20){
+//     console.log("The person is a teenager ")
+// } else if (age < 65){
+//     console.log("The person is adult ")
+// } else {
+//     console.log("The person is Elder")
+// }
+// if the person is at least 4, but less than 13,
+
+// let age: number = 12;
+// if (age < 2){
+//     console.log("The person is a Baby")
+// } else if (age < 4){
+//     console.log("The person is Toddler")
+// } else if (age < 13){
+//     console.log("The person is a kid")
+// } else if (age < 20){
+//     console.log("The person is a teenager ")
+// } else if (age < 65){
+//     console.log("The person is adult ")
+// } else {
+//     console.log("The person is Elder")
+// }
+
+//if the person is atleat 13'but less than 20
+
+// let age: number = 19;
+// if (age < 2){
+//     console.log("The person is a Baby")
+// } else if (age < 4){
+//     console.log("The person is Toddler")
+// } else if (age < 13){
+//     console.log("The person is a kid")
+// } else if (age < 20){
+//     console.log("The person is a teenager ")
+// } else if (age < 65){
+//     console.log("The person is adult ")
+// } else {
+//     console.log("The person is Elder")
+// }
+
+// if the person is at least 20, but less then 65
+// let age: number = 35;
+// if (age < 2){
+//     console.log("The person is a Baby")
+// } else if (age < 4){
+//     console.log("The person is Toddler")
+// } else if (age < 13){
+//     console.log("The person is a kid")
+// } else if (age < 20){
+//     console.log("The person is a teenager ")
+// } else if (age < 65){
+//     console.log("The person is adult ")
+// } else {
+//     console.log("The person is Elder")
+// }
+
+//if the person is 65 or older,
+
+// let age: number = 65;
+// if (age < 2){
+//     console.log("The person is a Baby")
+// } else if (age < 4){
+//     console.log("The person is Toddler")
+// } else if (age < 13){
+//     console.log("The person is a kid")
+// } else if (age < 20){
+//     console.log("The person is a teenager ")
+// } else if (age < 65){
+//     console.log("The person is adult ")
+// } else {
+//     console.log("The person is Elder")
+// }
+
+// Quetion no 29
+
+// let favourite_friuts: string[] = ["Mango","kiwi","cherry","Apple","orange"]
+
+// if (favourite_friuts.includes("Mango"))
+// console.log("Mango");
+
+// if (favourite_friuts.includes("Kiwi"))
+// console.log("Kiwi")
+
+// if (favourite_friuts.includes("cherry"))
+// console.log("cherry")
+
+// if (favourite_friuts.includes("Apple"))
+// console.log("I really like bananas")
+
+// if(favourite_friuts.includes("orange"))
+// console.log("I really like bananas")
+
+
+//Question no 30
+
+// let users : string [] = ["admin","eric","haseeb","ali","fatima"]
+
+//  for(let user of users){
+//     if (user === "admin")  {
+//         console.log("Hello admin,would you like to see a status report?")
+//     } else {
+//         console.log(`Helo ${user},thank you for logging in again`)
+//     }
+//  }
+
+
+
+
+
